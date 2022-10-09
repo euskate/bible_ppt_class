fb7db74e930617ad925fdf0c58e5351af9ff3cb0
euskate <euskate07@gmail.com>
2022-10-09 22:11:16 +0900

    add manual and studio

--- PATH.py
+++ PATH.py
@@ -6,8 +6,8 @@ app_path = os.path.dirname(os.path.abspath(__file__))
 SOURCE_PPT_PATH = os.path.join(app_path, r"오전예배_source.pptx")
 OUTPUT_SAVE_PATH = os.path.join(desktop_path, r"오전예배.pptx")
 
-RESPONSIVE_READING_PATH = r"C:\Users\giveroot\Documents\주의길PPT\교독문\주의길_교독문"
-HYMN_PATH = r"C:\Users\giveroot\Documents\주의길PPT\새찬송가16x9"
+RESPONSIVE_READING_PATH = r"F:\예배자료\교독문\주의길_교독문"
+HYMN_PATH = r"F:\예배자료\새찬송가16x9"
 
 # # test code
 # print(SOURCE_PPT_PATH)
